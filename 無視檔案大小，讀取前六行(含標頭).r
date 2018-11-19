a8f919ea67819a5dee5c2606505c65975dffc34e
呂豪笙 <reganlu007@gmail.com>
2018-11-19 10:27:56 +0800

    Update 無視檔案大小，讀取前六行(含標頭).r

--- 無視檔案大小，讀取前六行(含標頭).r
+++ 無視檔案大小，讀取前六行(含標頭).r
@@ -1,6 +1,6 @@
 readHead = function(x){
     pt   = file(x, "r")
-    name = strsplit(readLines(pt, 1), split=',')[[1]]
-    data = read.table(text=readLines(pt, 6), sep=',', col.names=name)
+    name = strsplit  (readLines(pt, 1), split=',')[[1]]
+    data = read.table(text = readLines(pt, 6), sep=',', col.names = name)
     data
 }
