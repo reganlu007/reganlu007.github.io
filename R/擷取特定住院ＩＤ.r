fb0d287dc953cae4588ad20cdc7a7455ec3ea3fb
呂豪笙 <reganlu007@gmail.com>
2018-12-04 10:15:43 +0800

    Update 擷取特定住院ＩＤ.r

--- R/擷取特定住院ＩＤ.r
+++ R/擷取特定住院ＩＤ.r
@@ -5,11 +5,11 @@ tmp2 = fread('子宮肌瘤-呂豪笙\\住診批價基本檔.csv')
 tmp3 = fread('子宮肌瘤-呂豪笙\\住診醫囑檔.csv')[,c(-32,-19)]
 tmp4 = fread('子宮肌瘤-呂豪笙\\住診藥囑檔.csv')[,c(-48,-44,-39,-28)]
 
-fw = function(x = c(850,892), y, z) fwrite(merge(unique(fread('子宮肌瘤住院病患_05_14.csv')[,1])[x], y), z)
-fw(tmp1,'_1.csv')
-fw(tmp2,'_2.csv')
-fw(tmp3,'_3.csv')
-fw(tmp4,'_4.csv')
+fw = function(x = '', y, z) fwrite(merge(unique(fread('子宮肌瘤住院病患_05_14.csv')[,1])[x], y), z)
+fw(c(850, 892), tmp1,'_1.csv')
+fw(c(850, 892), tmp2,'_2.csv')
+fw(c(850, 892), tmp3,'_3.csv')
+fw(c(850, 892), tmp4,'_4.csv')
 
 tmp5 = fread('子宮肌瘤-呂豪笙\\門診批價明細檔.csv')
 tmp6 = fread('子宮肌瘤-呂豪笙\\門診批價基本檔.csv')
