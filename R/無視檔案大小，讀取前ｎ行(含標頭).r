be3095d5d6590983a6f3dde34874caceaf7314de
呂豪笙 <reganlu007@gmail.com>
2018-12-04 10:13:24 +0800

    Update 無視檔案大小，讀取前ｎ行(含標頭).r

--- R/無視檔案大小，讀取前ｎ行(含標頭).r
+++ R/無視檔案大小，讀取前ｎ行(含標頭).r
@@ -1,5 +1,5 @@
-read = function(a, n=6){
+read = function(a, n = 6){
     x = file(a, "r")
     y = strsplit  (readLines(x, 1), split=',')[[1]]
-    z = read.table(text = readLines(x, n), sep=',', col.names = y);z
+    z = read.table(text = readLines(x, n), sep=',', col.names = y); z
 }
