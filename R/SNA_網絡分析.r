8c4c62e5351369502eea37f9172426f1185feb07
呂豪笙 <reganlu007@gmail.com>
2018-12-05 23:03:23 +0800

    Update SNA_網絡分析.r

--- R/SNA_網絡分析.r
+++ R/SNA_網絡分析.r
@@ -1,10 +1,15 @@
 library(data.table);library(magrittr);library(igraph)
 
-門0 = fread('門.csv')[,c(-3,-2,-1)]
+門0 = fread('門.csv')
+門1 = [grep('D31894D58D22CDECC5A55826F57B8DB1A6EE82CC', 主治醫師代號)][,c(-3,-2,-1)]
+門2 = [grep('3495F5EE004CF696D29F9FB0C0240EC3294EBFB9', 主治醫師代號)][,c(-3,-2,-1)]
+門3 = [grep('F01E3845A2DB53B44940D2E256DB96BC48FF1EFB', 主治醫師代號)][,c(-3,-2,-1)]
+門0 = 門0[,c(-3,-2,-1)]
+
 住0 = fread('住.csv')
-住1 = 住0[grep('E9385CAAB8C5A00B',醫師別)][,c(-3,-2,-1)]
-住2 = 住0[grep('F4262C7C21D08356',醫師別)][,c(-3,-2,-1)]
-住3 = 住0[grep('3941BF4DECDAE0F9',醫師別)][,c(-3,-2,-1)]
+住1 = 住0[grep('E9385CAAB8C5A00B', 醫師別)][,c(-3,-2,-1)]
+住2 = 住0[grep('F4262C7C21D08356', 醫師別)][,c(-3,-2,-1)]
+住3 = 住0[grep('3941BF4DECDAE0F9', 醫師別)][,c(-3,-2,-1)]
 住0 = 住0[,c(-3,-2,-1)]
 
 SNA = function(x){x[x>=1] = 1; graph.adjacency(t(x %<>% data.matrix) %*% x, weighted = T, mode = 'undirected')%>% simplify %>% return}
