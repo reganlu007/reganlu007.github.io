87c21277776fd66d13915c946b82b51c2190828c
呂豪笙 <reganlu007@gmail.com>
2018-12-05 20:04:21 +0800

    Update SNA_網絡分析.r

--- R/SNA_網絡分析.r
+++ R/SNA_網絡分析.r
@@ -3,19 +3,18 @@ load('termDocMatrix.rdata')
 termDocMatrix
 termDocMatrix[termDocMatrix>=1] = 1
 x = data.matrix(termDocMatrix)
-
-g = simplify(graph.adjacency((x %*% t(x)), weighted=T, mode = 'undirected'))
+x = x %*% t(x)
+x
+g = simplify(graph.adjacency(x), weighted=T, mode = 'undirected'))
 V(g)$label = V(g)$name
 V(g)$degree = degree(g)
 set.seed(3952)
 plot(g, layout=layout.fruchterman.reingold)
 plot(g, layout=layout.kamada.kawai)
-
 V(g)$label.cex = 2.2 * V(g)$degree / max(V(g)$degree)+ .2
 V(g)$label.color = rgb(0, 0, .2, .8)
 V(g)$frame.color = NA
 egam = (log(E(g)$weight)+.4) / max(log(E(g)$weight)+.4)
 E(g)$color = rgb(.5, .5, 0, egam)
 E(g)$width = egam
-
 plot(g, layout=layout.fruchterman.reingold)
