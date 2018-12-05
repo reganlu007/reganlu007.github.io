e814041c7b831200b22d28f821ed994c4c8473f8
呂豪笙 <reganlu007@gmail.com>
2018-12-05 22:00:39 +0800

    Update SNA_網絡分析.r

--- R/SNA_網絡分析.r
+++ R/SNA_網絡分析.r
@@ -1,15 +1,11 @@
-library(data.table);library(igraph)
+library(data.table);library(magrittr);library(igraph)
 
 a = fread('門0.csv')[,c(-2,-1)]
 b = fread('住0.csv')[,c(-3,-2,-1)]
 
-SNA = function(x){
-  x[x>=1]=1
-  x = data.matrix(x)
-  return(simplify(graph.adjacency((t(x) %*% x), weighted = T, mode = 'undirected')))
-}
+SNA = function(x){x[x>=1] = 1; graph.adjacency(t(x %<>% data.matrix) %*% x, weighted = T, mode = 'undirected')%>% simplify %>% return}
 PIC = function(g){
-  V(g)$label = V(g)$name
+  V(g)$label  = V(g)$name
   V(g)$degree = degree(g)
   set.seed(1)
   plot(g, layout=layout.kamada.kawai)
@@ -17,7 +13,7 @@ PIC = function(g){
 }
 PIC_2 = function(g){
   g = PIC(g)
-  V(g)$label.cex = 2.2 * V(g)$degree / max(V(g)$degree)+ .2
+  V(g)$label.cex   = 2.2 * V(g)$degree / max(V(g)$degree)+ .2
   V(g)$label.color = rgb(0, 0, .2, .8)
   V(g)$frame.color = NA
   egam = (log(E(g)$weight)+.4) / max(log(E(g)$weight)+.4)
