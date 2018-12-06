3a5f7246fb611f9e65ee74d9e61252c2be2271d3
呂豪笙 <reganlu007@gmail.com>
2018-12-07 00:40:12 +0800

    Update 照過超音波的病患.r

--- R/照過超音波的病患.r
+++ R/照過超音波的病患.r
@@ -1,5 +1,4 @@
-library(data.table);library(magrittr)
-id = fread('確診為子宮肌瘤之歸戶代號.csv')
+library(data.table); library(magrittr); id = fread('確診為子宮肌瘤之歸戶代號.csv')
 tmp1 = merge(id,fread('子宮肌瘤-呂豪笙\\門診批價明細檔.csv'))[grep('^S49-10', 收費編號)]
 tmp1
 tmp2 = merge(id,fread('子宮肌瘤-呂豪笙\\住診批價明細檔.csv'))[grep('^S49-10', 收費編號)]
