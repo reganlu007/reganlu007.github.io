b4ffda9b7806f1d8ac2b639cc3f5d6878c2fdbbd
呂豪笙 <reganlu007@gmail.com>
2018-12-07 00:37:39 +0800

    Create 超音波.r

---
+++ R/超音波.r
@@ -0,0 +1,6 @@
+library(data.table);library(magrittr)
+id = fread('確診為子宮肌瘤之歸戶代號.csv')
+
+# 住院
+tmp1 = merge(id,fread('子宮肌瘤-呂豪笙\\門診批價明細檔.csv'))[grep('^S49-10', 收費編號)]
+tmp2 = merge(id,fread('子宮肌瘤-呂豪笙\\住診批價明細檔.csv'))[grep('^S49-10', 收費編號)]
