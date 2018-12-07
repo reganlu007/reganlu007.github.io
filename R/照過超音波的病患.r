f98fffc12bfaea400d86a68b267855d545bf67e9
呂豪笙 <reganlu007@gmail.com>
2018-12-07 18:55:15 +0800

    Update 照過超音波的病患.r

--- R/照過超音波的病患.r
+++ R/照過超音波的病患.r
@@ -1,8 +1,11 @@
 library(data.table)
-id  = fread('確診為子宮肌瘤之歸戶代號.csv')
-tmp = fread('子宮肌瘤-呂豪笙\\門診批價明細檔.csv')[,-4]
-fwrite(merge(id,tmp[,-4])[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'門診超音波.csv')
-fwrite(merge(id,fread('門診批價明細檔_1.csv')[,-4])[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'門院超音波_1.csv')
-fwrite(merge(id,fread('門診批價明細檔_2.csv')[,-4])[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'門院超音波_2.csv')
-fwrite(merge(id,fread('門診批價明細檔_3.csv')[,-4])[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'門院超音波_3.csv')
-fwrite(merge(id,fread('住診批價明細檔.csv')       )[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'住院超音波.csv')
+f = function(x,y) fwrite(merge(fread('確診為子宮肌瘤之歸戶代號.csv'),x)[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],y)
+f(fread('門診批價明細檔_1.csv')[,-4],'門診超音波_1.csv')
+f(fread('門診批價明細檔_2.csv')[,-4],'門診超音波_2.csv')
+f(fread('門診批價明細檔_3.csv')[,-4],'門診超音波_3.csv')
+f(fread('住診批價明細檔.csv'),'住診超音波.csv')
+
+fwrite(merge(fread('確診為子宮肌瘤之歸戶代號.csv'),fread('門診批價明細檔_1.csv')[,-4])[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'門診超音波_1.csv')
+fwrite(merge(fread('確診為子宮肌瘤之歸戶代號.csv'),fread('門診批價明細檔_2.csv')[,-4])[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'門診超音波_2.csv')
+fwrite(merge(fread('確診為子宮肌瘤之歸戶代號.csv'),fread('門診批價明細檔_3.csv')[,-4])[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'門診超音波_3.csv')
+fwrite(merge(fread('確診為子宮肌瘤之歸戶代號.csv'),fread('住診批價明細檔.csv')       )[grep('^S49-101|^S49-104|^S49-104P|^S49-111', 收費編號)],'住診超音波.csv')
