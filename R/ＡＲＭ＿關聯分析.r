f849545b21cedd577892f4ed413daef8ff24aa8c
呂豪笙 <reganlu007@gmail.com>
2018-12-15 16:57:49 +0800

    Update ＡＲＭ＿關聯分析.r

--- R/ＡＲＭ＿關聯分析.r
+++ R/ＡＲＭ＿關聯分析.r
@@ -8,13 +8,13 @@ SORT = function(x) data.table(table(x))[N>0][order(-N)]
 
 # 樞紐分析
 # 檢視 support = 0.01, confidence = 0.8 的關聯分析結果
-ARM((tmp = dcast.data.table(fread('子宮肌瘤門診病患_05_14.csv'), 門診號+批價日期+主治醫師代號~收費編號))[,c(-3,-2,-1)], .01, .8)
+ARM((tmp = dcast.data.table(fread('子宮肌瘤門診中藥_醫師.csv'), 門診號+批價日期+主治醫師代號~收費編號))[,c(-3,-2,-1)], .01, .8)
 ARM( tmp[grep(門診醫師計數[1,1],主治醫師代號)][,c(-3,-2,-1)], .01, .8)
 ARM( tmp[grep(門診醫師計數[2,1],主治醫師代號)][,c(-3,-2,-1)], .01, .8)
 ARM( tmp[grep(門診醫師計數[3,1],主治醫師代號)][,c(-3,-2,-1)], .01, .8)
 
 # 檢視 support = 0.1 , confidence = 0.8 的關聯分析結果
-ARM((tmp = dcast.data.table(fread('子宮肌瘤住診病患_05_14.csv'), 住院號+日期+醫師別~收費編號))[,c(-3,-2,-1)])
+ARM((tmp = dcast.data.table(fread('子宮肌瘤住診中藥_醫師.csv'), 住院號+日期+醫師別~收費編號))[,c(-3,-2,-1)])
 ARM( tmp[grep(住院醫師計數[1,1],醫師別)][,c(-3,-2,-1)])
 ARM( tmp[grep(住院醫師計數[2,1],醫師別)][,c(-3,-2,-1)])
 ARM( tmp[grep(住院醫師計數[3,1],醫師別)][,c(-3,-2,-1)])
