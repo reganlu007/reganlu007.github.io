484ccaba03dde5015cc1ed2385f2b6c6a63709b9
呂豪笙 <reganlu007@gmail.com>
2018-12-16 21:24:28 +0800

    Update 平均每人每年花費.r

--- R/平均每人每年花費.r
+++ R/平均每人每年花費.r
@@ -1,22 +1,16 @@
 library(data.table);library(magrittr);library(dplyr)
-# 門診用
 
-tmp門診中藥 = fread('子宮肌瘤門診中藥.csv') %>% group_by(歸戶代號) %>% summarise(
-	sum(as.numeric(本院收費金額)), sum(as.numeric(病患自付金額)), sum(as.numeric(健保給付金額)), min(as.numeric(資料年月)), max(as.numeric(資料年月)))
-tmp門診中藥
-fwrite(tmp門診中藥,'門診中藥.csv')
+門診 = function(x) fread(x) %>% group_by(歸戶代號) %>% summarise(sum(as.numeric(本院收費金額)), sum(as.numeric(病患自付金額)), sum(as.numeric(健保給付金額)), min(as.numeric(批價日期)), max(as.numeric(批價日期))) %>% data.table
+住診 = function(x) fread(x) %>% group_by(歸戶代號) %>% summarise(sum(as.numeric(自付金額)), sum(as.numeric(健保金額)), min(as.numeric(批價日期)), max(as.numeric(批價日期)))
 
-tmp門診西藥 = fread('子宮肌瘤門診西藥.csv') %>% group_by(歸戶代號) %>% summarise(
-	sum(as.numeric(本院收費金額)), sum(as.numeric(病患自付金額)), sum(as.numeric(健保給付金額)), min(as.numeric(資料年月)), max(as.numeric(資料年月)))
-tmp門診西藥
-fwrite(tmp門診西藥,'門診西藥.csv')
+tmp門診中藥 = 門診('子宮肌瘤門診中藥.csv')
+tmp門診中藥 %>% fwrite('門中金額.csv')
 
-# 住診用
+tmp門診西藥 = 門診('子宮肌瘤門診西藥.csv')
+tmp門診西藥 %>% fwrite('門西金額.csv')
 
-tmp住診中藥 = fread('子宮肌瘤住診中藥.csv') %>% group_by(歸戶代號) %>% summarise(sum(as.numeric(自付金額)), sum(as.numeric(健保金額)), min(as.numeric(資料年月)), max(as.numeric(資料年月)))
-tmp住診中藥
-fwrite(tmp住診中藥,'住診中藥.csv')
+tmp住診中藥 = 住診('子宮肌瘤住診中藥.csv')
+tmp住診中藥 %>% fwrite('住中金額.csv')
 
-tmp住診西藥 = fread('子宮肌瘤住診西藥.csv') %>% group_by(歸戶代號) %>% summarise(sum(as.numeric(自付金額)), sum(as.numeric(健保金額)), min(as.numeric(資料年月)), max(as.numeric(資料年月)))
-tmp住診西藥
-fwrite(tmp住診西藥,'住診西藥.csv')
+tmp住診西藥 = 住診('子宮肌瘤住診西藥.csv')
+tmp住診西藥 %>% fwrite('住西金額.csv')
