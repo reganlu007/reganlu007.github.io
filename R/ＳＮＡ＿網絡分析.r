cb734b3a07299f10b4b9f1c8d76414cf08499ba8
呂豪笙 <reganlu007@gmail.com>
2019-01-03 23:24:55 +0800

    Update ＳＮＡ＿網絡分析.r

--- R/ＳＮＡ＿網絡分析.r
+++ R/ＳＮＡ＿網絡分析.r
@@ -19,10 +19,21 @@ CFG = function(g, m=cluster_optimal(g),v=degree(g),e=E(g)$weight,l=layout.circle
 
 
 
-(g=(住0%>%SET_住)[,c(1,7:9,11,14,16,17,21,23,25,27,29,31, 2:6,10,12,15,18:20,22,24,26,28,30,32, 13,33:37)]%>%SNA)%>%CFG(m=cluster_optimal(g),v=degree(g)^.3*9,e=E(g)$weight^.5)
-(g=(住1%>%SET_住)[,c(35,8,9,11,14,16,17,21,6,20,22,23,25,26,29,5,33,34)]%>%SNA)%>%CFG(m=cluster_optimal(g),v=degree(g)^.4*15,e=E(g)$weight^.4)
-(g=(住2%>%SET_住)[,c(4,6,14,24,33:37)]%>%SNA)%>%CFG(m=cluster_optimal(g),v=degree(g)*15,e=E(g)$weight^.5)
-(g=(住3%>%SET_住)[,c(16,17,23,25,29,33,35,36,37,3,14)]%>%SNA)%>%CFG(v=degree(g)^.6*25,e=E(g)$weight^.5*2)
+(g=(住0%>%SET_住)[,c(1,7:9,11,14,16,17,21,23,25,27,29,31,
+		   2:6,10,12,15,18:20,22,24,26,28,30,32,
+		   13,33:37)]%>%SNA)
+	%>%CFG(v=degree(g)^.3*9,e=E(g)$weight^.5)
+(g=(住0%>%SET_住)[,c(1,7:9,11,14,16,17,21,23,25,27,29,31)]%>%SNA)
+	%>%plot(vertex.size=degree(g)^.5*10,edge.width=E(g)$weight^.2*2,layout=layout.circle)
+(g=(住0%>%SET_住)[,c(13,33:37)]%>%SNA)
+	%>%plot(vertex.size=degree(g)*10,edge.width=E(g)$weight^.5,layout=layout.circle)
+
+(g=(住1%>%SET_住)[,c(35,8,9,11,14,16,17,21,6,20,22,23,25,26,29,5,33,34)]%>%SNA)
+	%>%CFG(v=degree(g)^.4*15,e=E(g)$weight^.4)
+(g=(住2%>%SET_住)[,c(4,6,14,24,33:37)]%>%SNA)
+	%>%CFG(v=degree(g)*15,e=E(g)$weight^.5)
+(g=(住3%>%SET_住)[,c(16,17,23,25,29,33,35,36,37,3,14)]%>%SNA)
+	%>%CFG(v=degree(g)^.6*25,e=E(g)$weight^.5*2)
 
 CEN = function(x) data.table(rbind(
   cbind(centr_degree(x)$centralization ,
