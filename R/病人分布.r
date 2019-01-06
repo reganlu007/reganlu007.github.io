490142fe1502b745de818db4483a21a4bcacc5eb
呂豪笙 <reganlu007@gmail.com>
2019-01-06 15:42:48 +0800

    Update 病人分布.r

--- R/病人分布.r
+++ R/病人分布.r
@@ -1,9 +1,5 @@
 library(data.table);library(magrittr);library(dplyr)
 
-# 住院天數統計 
-### 先用 excel 讀取 住院基本檔 後 轉換日期為 date 格式 重整住院天數 再用 R 篩選出整理好的歸戶 ID
-sum((tmp = merge(住診, 住診批價基本))$住院天數)/length(unique(tmp$歸戶代號))
-
 icd_9 = '^218|^617|^6253|^6254|^626|^628'
 住院申報 = fread('子宮肌瘤-呂豪笙\\住院申報費用清單.csv')
 rbind(	住院申報[grep(icd_9,主疾病代號)],
@@ -18,3 +14,5 @@ rbind(	門診歷史[grep(icd_9,疾病代號一)],
 	門診歷史[grep(icd_9,疾病代號四)],
 	門診歷史[grep(icd_9,疾病代號五)],
 	門診歷史[grep(icd_9,疾病代號六)]) %>% unique %>% fwrite('門診處方歷史檔_icd_selected.csv')
+tmp1.1 = merge(fread('門診處方歷史檔_確診ID.csv'),fread('門診處方歷史檔_icd_selected.csv'))
+tmp2.1 = merge(fread('住院申報費用清單_確診ID.csv'),fread('住院申報費用清單_icd_selected.csv'))
