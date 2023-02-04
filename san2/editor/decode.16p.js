9662d5f5a59be559c463c6edc5af03438edaa9d1
呂豪笙 <reganlu007@gmail.com>
2023-02-04 22:32:24 +0800

    Add files via upload

--- san2/editor/decode.16p.js
+++ san2/editor/decode.16p.js
@@ -1,7 +1,9 @@
-//	decode_16p 破解函式
+//	msg_16p name_16p 破解
 //	mod = 0 or false : read name.16p
 //	mod = 1 or  true : read  msg.16p
-	decode_16p =  (arr,dic,mod) => {tmp = {0: arr, 'c': 16, 'r': mod + 14};
+	dec_16p =  (dic,arr,mod) => {
+	mod = ['name_16p', 'msg_16p'].indexOf(mod);
+	tmp = { 0: arr, 'c' : 16, 'r' : mod + 14 };
 	tmp[1]= tmp[0].map(x => x.toString(2).padStart(8,0)) + '';
 	tmp[2]= tmp[1].replace(/,/g, '').split('');
 	tmp.r = [...Array(tmp.r).keys()];
@@ -15,11 +17,17 @@
 	tmp[3][n][r][c] =   !!mod && r == 0 && c < 16 ? tmp[3][n][r][c] * true : tmp[3][n][r][c] == 0 ? '' : '圖';
 	}); /* c */
 	tmp[3][n].value = [dic[n] || '', '']; tmp.c.map(c => {
-	tmp[3][n].value[1] += !!mod ? tmp[3][n][0][c] : '';
+	tmp[3][n].value[1]+=!!mod ?  tmp[3][n][0][c] : '';
 	}); /* c */
 	});	/* r */
 	});	/* n */
 	tmp =  tmp[3];	//	去蕪存菁
 	return tmp;
-};	get_name_16p = () => decode_16p(name_16p, name_16p_dic, 0); see_name_16p =(i) => console.table(get_name_16p()[i]);
-	get__msg_16p = () => decode_16p( msg_16p,  msg_16p_dic, 1); see__msg_16p =(i) => console.table(get__msg_16p()[i]);
+};	get_name_16p = () => dec_16p(name_16p_dic,name_16p,'name_16p'); see_name_16p =(i) => console.table(get_name_16p()[i]);
+	get__msg_16p = () => dec_16p( msg_16p_dic, msg_16p, 'msg_16p'); see__msg_16p =(i) => console.table(get__msg_16p()[i]);
+
+	dec_kaodata	= () => {
+	return 'not yet.';
+};	get_kaodata	= () => {
+	return 'not yet.';
+}
