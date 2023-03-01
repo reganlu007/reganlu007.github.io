670823c9ab7936b94bdfe3d01d0b029efc9da438
呂豪笙 <reganlu007@gmail.com>
2023-03-01 12:57:11 +0800

    Update decode.16p.js

--- san2/editor/decode.16p.js
+++ san2/editor/decode.16p.js
@@ -17,23 +17,12 @@ for(var n = 0 ; n  < tmp[5]; n++){
 };	get_name_16p = dec_16p(name_16p_dic,name_16p,'name_16p');
 	get__msg_16p = dec_16p( msg_16p_dic, msg_16p, 'msg_16p');
 //	man  kaodata 破解
-	dec__kaodata	= (arr) => {
-	tmp = {};
-	tmp[0]= arr;
-	tmp[1]=	tmp[0].map(x => x.toString(2).padStart(8,0)).join('');
-	tmp[2]= [];
-	tmp.r = 24;
-	[...Array(tmp[1].length / tmp.r).keys()].map(x => tmp[2].push(tmp[1].slice(tmp.r * x, tmp.r * x + tmp.r)));
-	tmp[3]= [];   tmp[2].map(x => {
-	[...Array(8).keys()].map(y => {
-	tmp[4]= ''; [...Array(3).keys()].map(z => tmp[4]+= x[y + 8 * z]);
-	tmp[4]= parseInt(tmp[4], 2);
-	tmp[3].push(tmp[4]);
-	}); /* c */
-	}); /* r */
-	tmp          = tmp[3].join('');	//	去蕪存菁
+	__code_kaodata=(arr,row,col)=>{tmp={};tmp[0]=arr;tmp[1]=tmp[0].map(x => Number(x).toString(2).padStart(row,0)).join('');tmp[2]=[];[...Array(tmp[1].length/24).keys()].map(x => tmp[2].push(tmp[1].slice(24*x,24*x+24)));tmp[3]=[];
+	tmp[2].map(x=>{[...Array(row).keys()].map(y =>{tmp[4]='';[...Array(col).keys()].map(z => tmp[4]+= x[y+row*z]);tmp[4]=parseInt(tmp[4],2);tmp[3].push(tmp[4]);});});tmp=tmp[3];
 	return	tmp;
-};	get__kaodata = dec__kaodata(kaodata_dat);
+};	decode_kaodata=(x)=>__code_kaodata(x,8,3);
+	encode_kaodata=(x)=>__code_kaodata(x,3,8);
+	get__kaodata = decode_kaodata(kaodata_dat);
 	dec__hexdata = (arr) => {
 	tmp = {};
 	[...Array(41).keys()].map(x => {
@@ -42,5 +31,5 @@ for(var n = 0 ; n  < tmp[5]; n++){
 		tmp[x].出陣 = arr.slice(6396 + 156 * x, 6396 + 156 * x + 156);
 	});
 	return	tmp;
-};
-	get__hexdata = dec__hexdata(hexdata_dat);
+};	get__hexdata = dec__hexdata(hexdata_dat);
+
