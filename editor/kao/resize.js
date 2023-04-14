6116ce481a13f0338195309d26d639bd2f5821b7
呂豪笙 <reganlu007@gmail.com>
2023-04-14 14:38:13 +0800

    Update resize.js

--- editor/kao/resize.js
+++ editor/kao/resize.js
@@ -1,2 +1,4 @@
 //  延展成為正常頭像 (abc => aabbcc)
-resize_from_1_to_2=(x)=>x.replace(/.{48}/g,'$&,').split(',').map(x=>x+','+x).toString().replace(/,/g,'\n');
+resize_by_regan = x => x.replace(/.{48}/g,'$&,').split(',').map(x=>x+','+x).toString().replace(/,/g,'\n');
+//  大神指點簡化語法
+resize_by_yuxio = x => x.  match(/.{48}/g).map(l => `${l}\n${l}`).join('\n');
