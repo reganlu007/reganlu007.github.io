dacc6b770a06d8104d8eb6fe59e8b93c584b012a
呂豪笙 <reganlu007@gmail.com>
2023-04-14 15:09:16 +0800

    Update resize.js

--- editor/kao/resize.js
+++ editor/kao/resize.js
@@ -7,7 +7,7 @@ resize_by_yuxio_1 = x => x.  match(  /.{48}/g).map(l => `${l}\n${l}`).join('\n')
 //  大神指點簡化語法
 resize_by_yuxio_2 = x => x.  match(/.{1,48}/g).map(l => `${l}\n${l}`).join('\n');
 //  測試方法
-console.log(resize_by_regan_0(str));
-console.log(resize_by_yuxio_1(str));
-console.log(resize_by_yuxio_2(str));
+console.log('resize_by_regan_0 :\n\n' + resize_by_regan_0(str));
+console.log('resize_by_yuxio_1 :\n\n' + resize_by_yuxio_1(str));
+console.log('resize_by_yuxio_2 :\n\n' + resize_by_yuxio_2(str));
 
