edeb78d5927a2a0f49c5434095ff055203f63f6e
呂豪笙 <reganlu007@gmail.com>
2023-04-14 14:44:38 +0800

    Update resize.js

--- editor/kao/resize.js
+++ editor/kao/resize.js
@@ -1,4 +1,13 @@
+//  範例字串
+str = '1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890';
 //  延展成為正常頭像 (abc => aabbcc)
-resize_by_regan = x => x.replace(/.{48}/g,'$&,').split(',').map(x=>x+','+x).toString().replace(/,/g,'\n');
+resize_by_regan_0 = x => x.replace(  /.{48}/g,'$&,').split(',').map(x=>x+','+x).toString().replace(/,/g,'\n');
 //  大神指點簡化語法
-resize_by_yuxio = x => x.  match(/.{48}/g).map(l => `${l}\n${l}`).join('\n');
+resize_by_yuxio_1 = x => x.  match(  /.{48}/g).map(l => `${l}\n${l}`).join('\n');
+//  大神指點簡化語法
+resize_by_yuxio_2 = x => x.  match(/.{1,48}/g).map(l => `${l}\n${l}`).join('\n');
+//  測試方法
+console.log(resize_by_regan_0(str));
+console.log(resize_by_yuxio_1(str));
+console.log(resize_by_yuxio_2(str));
+
